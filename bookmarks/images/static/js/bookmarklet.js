1408efbaeea4600b58047202ca0bfc028ea370b8
Travis Mullen <travis.a.mullen@gmail.com>
2025-04-24 11:14:24 -0400

    Completed Ch. 6. Bookmarklet button stopped working for some reason.

--- bookmarks/images/static/js/bookmarklet.js
+++ bookmarks/images/static/js/bookmarklet.js
@@ -1,25 +1,24 @@
 const siteUrl = '//127.0.0.1:8000/';
-const styleUrl = siteUrl + '/static/css/bookmarklet.css';
+const styleUrl = siteUrl + 'static/css/bookmarklet.css';
 const minWidth = 250;
 const minHeight = 250;
 
-// load css
-var head = document.getElementsByTagName('head')[0];
-var link = document.createElement('link');
-link.rel = 'stylesheet';
+// load CSS
+var head = document.getElementsByTagName('head')[0];  // Get HTML head element
+var link = document.createElement('link'); // Create new link Element
+link.rel = 'stylesheet'; // set the attributes for link element
 link.type = 'text/css';
-link.href = styleUrl + "?r=" + Math.floor(Math.random()*999999999999);
-head.appendChild(link);
+link.href = styleUrl + '?r=' + Math.floor(Math.random()*9999999999999999);
+head.appendChild(link);  // Append link element to HTML head
 
-// Load html
+// load HTML
 var body = document.getElementsByTagName('body')[0];
 boxHtml = `
-    <div id="bookmarklet">
-        <a href="#" id="close">&times</a>
-        <h1>Select an image to bookmark</h1>
-        <div class="images"></div>
-    </div>
-`;
+  <div id="bookmarklet">
+    <a href="#" id="close">&times;</a>
+    <h1>Select an image to bookmark:</h1>
+    <div class="images"></div>
+  </div>`;
 body.innerHTML += boxHtml;
 
 function bookmarkletLaunch() {
@@ -31,35 +30,37 @@ function bookmarkletLaunch() {
     // display bookmarklet
     bookmarklet.style.display = 'block';
 
-    bookmarklet.querySelector('#close').addEventListener('click', function() {
-        // e.preventDefault();
-        bookmarklet.style.display = 'none';
-    });
+    // close event
+    bookmarklet.querySelector('#close')
+        .addEventListener('click', function(){
+            bookmarklet.style.display = 'none'
+        });
 
-    // Find images in the DOM with the minimum dimensions
-    var images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]');
-    // images = Array.from(images);
+    // find images in the DOM with the minimum dimensions
+    images = document.querySelectorAll('img[src$=".jpg"], img[src$=".jpeg"], img[src$=".png"]');
     images.forEach(image => {
-        if (image.naturalWidth >= minWidth && image.naturalHeight >= minHeight) {
+        if(image.naturalWidth >= minWidth
+            && image.naturalHeight >= minHeight)
+        {
             var imageFound = document.createElement('img');
             imageFound.src = image.src;
-            console.log("Image:" + imageFound);
-            imagesFound.appendChild(imageFound);
+            imagesFound.append(imageFound);
         }
     })
 
+    // select image event
     imagesFound.querySelectorAll('img').forEach(image => {
-        image.addEventListener('click', function(event) {
-                imageSelected = event.target;
-                bookmarklet.style.display = 'none';
-                window.open(siteUrl + 'images/create/?url=' + encodeURIComponent(imageSelected.src)
-                                            + '&title=' + encodeURIComponent(document.title),
-                                            '_blank'
-                );
-            }
-        )
+        image.addEventListener('click', function(event){
+            imageSelected = event.target;
+            bookmarklet.style.display = 'none';
+            window.open(siteUrl + 'images/create/?url='
+                + encodeURIComponent(imageSelected.src)
+                + '&title='
+                + encodeURIComponent(document.title),
+                '_blank');
+        })
     })
 }
 
-// Launch the bookmarklet
+// launch the bookmkarklet
 bookmarkletLaunch();
